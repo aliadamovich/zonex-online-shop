67239c200e416678a826be5479d82bcf05e071f2
aliadamovich <alesya.argentina@gmail.com>
2024-01-27 19:45:16 -0300

    slider

--- src/js/app.js
+++ src/js/app.js
@@ -2,16 +2,23 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
 
-import Swiper from 'swiper';
-import Pagination from 'swiper/modules';
+// import Swiper from 'swiper';
+// import Pagination from 'swiper/modules';
 // import 'swiper/css'
 // import 'swiper/css/pagination';
 
-const swiperBanner = new Swiper('.cover__swiper', {
-	// loop: true,
-	// direction: 'vertical',
-	pagination: {
-		el: '.swiper-pagination',
-	},
+// const swiperBanner = new Swiper('.cover__swiper', {
 
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 	},
+
+// });
+
+
+const swiper = new Swiper('.cover__swiper', {
+	loop: true,
+		pagination: {
+			el: '.cover__pagination',
+		},
 });
