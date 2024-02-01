cd73206c5368e68aeed2da16396d01a95e3ebb95
aliadamovich <alesya.argentina@gmail.com>
2024-02-01 20:33:15 -0300

    filters

--- src/js/app.js
+++ src/js/app.js
@@ -20,70 +20,88 @@ const swiperCatalog = new Swiper('.cover-catalog__slider', {
 });
 
 //Marketing pop-up
-
 const marketing = document.querySelector('.marketing');
 
-let counter = 0;
-let delay = 10000;
+if (marketing) {
 
-const marketingData = [
-	{
-		photo: '../img/coat.png',
-		title: 'Faux shearling double-breasted coat',
-		where: 'London, Great Britain',
-		when: '15'
-	},
-	{
-		photo: '../img/oversize.png',
-		title: 'Oversize t-shirt',
-		where: 'New York, Usa',
-		when: '10'
-	},
-	{
-		photo: '../img/leather_shoes.png',
-		title: 'Black leather shoes',
-		where: 'Rome, Italy',
-		when: '7'
-	}
-]
+	let counter = 0;
+	let delay = 10000;
 
-marketing.addEventListener('click', (e) => {
-	if (e.target.classList.contains('marketing__close')) {
-		removeMarketing()
+	const marketingData = [
+		{
+			photo: '../img/coat.png',
+			title: 'Faux shearling double-breasted coat',
+			where: 'London, Great Britain',
+			when: '15'
+		},
+		{
+			photo: '../img/oversize.png',
+			title: 'Oversize t-shirt',
+			where: 'New York, Usa',
+			when: '10'
+		},
+		{
+			photo: '../img/leather_shoes.png',
+			title: 'Black leather shoes',
+			where: 'Rome, Italy',
+			when: '7'
+		}
+	]
+
+	marketing.addEventListener('click', (e) => {
+		if (e.target.classList.contains('marketing__close')) {
+			removeMarketing()
+		}
+	})
+
+
+	function removeMarketing() {
+		marketing.classList.add('__closed');
 	}
+
+	function changeMarketing() {
+
+		if (!marketing.classList.contains('__closed')) {
+			removeMarketing();
+		};
+
+		setTimeout(() => {
+			const productImage = document.querySelector('.img-marketing');
+			const productName = document.querySelector('.info-marketing__title');
+			const purchaseTime = document.querySelector('.info-marketing__when');
+			const purchasePlace = document.querySelector('.info-marketing__where');
+
+			productImage.src = marketingData[counter].photo;
+			productName.innerHTML = marketingData[counter].title;
+			purchaseTime.innerHTML = `${marketingData[counter].when} minutes ago`;
+			purchasePlace.innerHTML = `${marketingData[counter].where}`;
+
+			counter++;
+			if (counter >= marketingData.length) {
+				counter = 0;
+			}
+			marketing.classList.remove('__closed');
+		}, delay - (delay - 500));
+
+	}
+
+	changeMarketing();
+	// setInterval(changeMarketing, delay);
+}
+
+const filterBlock = document.querySelectorAll('.categories-filter__item');
+const hideFilters = document.querySelector('.hide-filter__btn');
+
+filterBlock.forEach(item => {
+	item.addEventListener('click', (e) => {
+		if (e.target.closest('.categories-filter__head')) {
+			item.classList.toggle('open');
+		}
+	})
 })
 
-
-function removeMarketing() {
-	marketing.classList.add('__closed');
-}
-
-function changeMarketing() {
-
-	if (!marketing.classList.contains('__closed')) {
-		removeMarketing();
-	};
-	
-	setTimeout(() => {
-		const productImage = document.querySelector('.img-marketing');
-		const productName = document.querySelector('.info-marketing__title');
-		const purchaseTime = document.querySelector('.info-marketing__when');
-		const purchasePlace = document.querySelector('.info-marketing__where');
-
-		productImage.src = marketingData[counter].photo;
-		productName.innerHTML = marketingData[counter].title;
-		purchaseTime.innerHTML = `${marketingData[counter].when} minutes ago`;
-		purchasePlace.innerHTML = `${marketingData[counter].where}`;
-
-		counter++;
-		if (counter >= marketingData.length) {
-			counter = 0;
-		}
-		marketing.classList.remove('__closed');
-	}, delay - (delay - 500));
-
-}
-
-changeMarketing();
-// setInterval(changeMarketing, delay);
-
+hideFilters.addEventListener('click', (e) => {
+	filterBlock.forEach(item => {
+		item.classList.remove('open');
+	})
+} )
