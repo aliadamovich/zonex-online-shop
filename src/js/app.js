6d9e8da45796ea56f86ceb1d288f01701394e582
aliadamovich <alesya.argentina@gmail.com>
2024-02-08 15:36:26 -0300

    second page adaptive start

--- src/js/app.js
+++ src/js/app.js
@@ -28,6 +28,28 @@ spaceCover.addEventListener('click', () => {
 	}
 })
 
+//*catalog-burger
+const iconCatalog = document.querySelector('.catalog__icon');
+const asideFilter = document.querySelector('.aside-filter');
+const buttonFilter = document.querySelector('.sort-grid__button-filter');
+if (buttonFilter) {
+	buttonFilter.addEventListener('click', function(e) {
+		
+		if (!asideFilter.classList.contains('_active')) {
+			asideFilter.classList.add('_active');
+			document.body.classList.add('_lock');
+		}
+	})
+}
+
+iconCatalog.addEventListener('click', () => {
+	if (asideFilter.classList.contains('_active')) {
+		asideFilter.classList.remove('_active');
+		document.body.classList.remove('_lock');
+	}
+})
+
+
 
 //*swiper
 const swiperMain = new Swiper('.cover__swiper', {
