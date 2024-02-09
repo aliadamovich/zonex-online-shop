bcf68a628d8eb2a758a7f55792b6505c7859ea03
aliadamovich <alesya.argentina@gmail.com>
2024-02-09 19:20:44 -0300

    select in footer

--- src/js/app.js
+++ src/js/app.js
@@ -240,7 +240,6 @@ if (customSelect.length > 0) {
 	customSelect.forEach(item => {
 		item.addEventListener('click', (e) => {
 			e.currentTarget.classList.toggle('custom-select--active')
-
 		if (e.target.classList.contains('custom-select__item')) {
 			let text = e.target.textContent;
 			e.currentTarget.querySelector('.custom-select__top').textContent = text;
