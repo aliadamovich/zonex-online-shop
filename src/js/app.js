18d79d0f302a46ebafd6824bbcf473ce6086e0d0
aliadamovich <alesya.argentina@gmail.com>
2024-02-10 22:27:22 -0300

    card product

--- src/js/app.js
+++ src/js/app.js
@@ -214,23 +214,28 @@ if (gridButtons) {
 	})
 }
 
-window.addEventListener('resize', resizeGallery);
+let catalogWrapper = document.querySelector('.catalog-wrapper');
 
-function resizeGallery(e) {
-	if (window.innerWidth <= 768 && window.innerWidth > 500) {
-		filterProducts.dataset.gridcolumns = 3;
-	} else if (window.innerWidth <= 500) {
-		filterProducts.dataset.gridcolumns = 2;
+if (catalogWrapper) {
+	window.addEventListener('resize', resizeGallery);
+
+	function resizeGallery(e) {
+		if (window.innerWidth <= 768 && window.innerWidth > 500) {
+			filterProducts.dataset.gridcolumns = 3;
+		} else if (window.innerWidth <= 500) {
+			filterProducts.dataset.gridcolumns = 2;
+		}
+		else {
+			const currentButtn = Array.from(gridButtons.querySelectorAll('.sort-grid__btn')).find(item =>
+				item.classList.contains('sort-grid__btn--current', 'nav__link--current')
+			);
+			filterProducts.dataset.gridcolumns = currentButtn.dataset.gridbtn;
+		}
 	}
-	else {
-		const currentButtn = Array.from(gridButtons.querySelectorAll('.sort-grid__btn')).find(item => 
-		item.classList.contains('sort-grid__btn--current', 'nav__link--current')
-	);
-		filterProducts.dataset.gridcolumns = currentButtn.dataset.gridbtn;
-	}
+
+	resizeGallery();
 }
 
-resizeGallery();
 
 //*select 'sort by'
 
@@ -410,20 +415,48 @@ function autoRemoveButton (parent, text) {
 }
 
 
-
+//!Картчока товара
+//выбор цвета 
 let colorBlock = document.querySelector('.color-info');
 
 if (colorBlock) {
 
 	colorBlock.addEventListener('click', (e) => {
-		colorBlock.querySelectorAll('.color-info__button');
-		console.log(colorBlock.querySelectorAll('.color-info__button'));
+	
+		const colorButtons = colorBlock.querySelectorAll('.color-info__button');
 		
-		// .forEach(item => {
-		// 	item.classList.remove('color-info__button--selected');
-		// })
+		colorButtons.forEach(item => {
+			item.classList.remove('color-info__button--selected');
+		})
 		if (e.target.classList.contains('color-info__button')) {
-			e.target.classList.add('color-info__button--selected')
+			e.target.classList.add('color-info__button--selected');
+			let textColor = e.target.dataset.color;
+			colorBlock.querySelector('.color-info__span').textContent = textColor;
+		}
+	})
+}
+//выбор размера 
+let sizeBlock = document.querySelector('.size-info');
+
+if (sizeBlock) {
+
+	sizeBlock.addEventListener('click', (e) => {
+		const sizeButtons = sizeBlock.querySelectorAll('.size-info__button');
+
+		sizeButtons.forEach(item => {
+			item.classList.remove('size-info__button--selected');
+		})
+		if (e.target.classList.contains('size-info__button')) {
+			e.target.classList.add('size-info__button--selected');
+			let textSize = e.target.textContent;
+			sizeBlock.querySelector('.size-info__span').textContent = textSize;
+		}
+		const sizeReset = sizeBlock.querySelector('.size-info__clear');
+		if (e.target.classList.contains('size-info__clear')) {
+			sizeButtons.forEach(item => {
+				item.classList.remove('size-info__button--selected');
+				sizeBlock.querySelector('.size-info__span').textContent = 'select a size';
+			})
 		}
 	})
 }
